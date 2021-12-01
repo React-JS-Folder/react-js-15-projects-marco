f0188a7082ee385c15b336b8685d1c4dacd22a7b
Marco <marco_trabajo@yahoo.com>
2021-12-01 14:59:21 +0100

    Create a Book component with 3 nested components, Image, Title, Author

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
-function Greetings() {
-  return <h4>My first component from empty react app</h4>
+
+//Create an amazon book cover 
+function BookList() {
+  return (
+    <section>
+      <Book />
+    </section>
+  )
 }
 
-ReactDom.render(<Greetings />, document.getElementById("root"));
+const Book = () => {
+  return (
+    <article>
+      <Image> </Image>
+      <Title />
+      <Author />
+    </article>
+  );
+
+};
+
+const Image = () => (
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" alt="" />
+);
+
+const Title = () => <h1>I love You to the Moon and Back</h1>
+const Author = () => <h4>Amelia Hepworth</h4>
+
+ReactDom.render(<BookList />, document.getElementById("root"));
