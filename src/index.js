e4df790ab2ab3bd458f8d52a93c5e429d22be97d
Marco <marco_trabajo@yahoo.com>
2021-12-02 13:47:09 +0100

    React JS Props - Passing values from parent component to direct child components

--- src/index.js
+++ src/index.js
@@ -3,41 +3,35 @@ import ReactDom from 'react-dom'
 import './index.css'  
 
 
-//Create an amazon book cover 
+//Props - pass values from parent to child components 
+ 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  )
-}
-
-const Book = () => {
   const title = "I love You to the Moon and Back";
   const author = "Amelia Hepworth";
   const img =  "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg" 
 
+  return (
+    <section className="booklist">
+      <Book img={img} title={title} author={author}/>  {/* You can pass only the props that you need */}
+      <Book author={author}/>                          {/* You can pass only the props that you need */}
+    </section>
+  )
+}
+
+const Book = (props) => {
 
   return (
     <article className="book">
-      <img 
-      src={img}
-      alt="" 
-      />
-      <h1>{title}</h1>      { /* the { } are to return the value of the expression */ }
-      <h4>{author.toUpperCase()}</h4>     { /* you can use string methods    author.toUpperCase() */}
-
-
+      <h1>{props.title}</h1>
+      <h4>{props.author}</h4>
+      <img src={props.img} alt=""/>
     </article>
   );
 
 };
+
+
+
+
 
 ReactDom.render(<BookList />, document.getElementById("root"));
