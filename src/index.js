c6a3e10e18868af2abb3a88d7b93493422b9e994
Marco <marco_trabajo@yahoo.com>
2021-12-02 20:56:18 +0100

    React JS - Create a component for each item using a map

--- src/index.js
+++ src/index.js
@@ -2,17 +2,50 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'  
 
+const books = [
+  {
+    img: 
+      'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth'
+  },
+  {
+    img: 
+      'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen'
+  }
+];
 
-const names = ['john', 'peter', 'susan'];
- 
-const newNames = names.map( (name) => {
-  return <h1>{name} </h1>
-});
-function BookList() {
+function BookList(){
+  return(
+    <section className="booklist">
+      {books.map( (book) => {
+        const {img, title, author} = book;
+        return (
+          <Book title={title} author={author} />
+        )
+      })}
+     
+    </section>
+  );
+}
+
+
+const Book = (props) => {
+  const {img, title, author} = props;
+
   return (
-    <section className="booklist"> {newNames} </section>
+    <article className="book">
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
+    </article>
   )
 }
+
+
+
 
 
 ReactDom.render(<BookList />, document.getElementById("root"));
