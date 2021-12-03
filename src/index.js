57a29f5e2614aa68ae3f35e34a716226608b363b
Marco <marco_trabajo@yahoo.com>
2021-12-03 21:04:28 +0100

    React JS - create a key for each map element

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css'
 
 const books = [
   {
+    id: 1,  //this is to create a key (react needs to register each map element)
     img: 
-      'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
     title: 'I Love You to the Moon and Back',
     author: 'Amelia Hepworth'
   },
   {
+    id: 2,  //this is to create a key (react needs to register each map element)
     img: 
       'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
     title: 'Our Class is a Family',
@@ -21,10 +23,8 @@ function BookList(){
   return(
     <section className="booklist">
       {books.map( (book) => {
-        const {img, title, author} = book;
         return (
-          // <Book title={title} author={author} />   // you take value by value, too slow. 
-          <Book book={book} />                        // you create an object and take all values from it 
+          <Book key={book.id} book={book} />   //for each map element print component with the key                 
         )
       })}
      
@@ -34,9 +34,7 @@ function BookList(){
 
 
 const Book = (props) => {
-  // const {img, title, author} = props;         //pulling values from parent components props --- slow 
-  const {img, title, author} = props.book;       //after creating an object in props --------------faster
-
+  const {img, title, author} = props.book;       
   return (
     <article className="book">
       <img src={img} alt="" />
