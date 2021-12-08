860fef9a19026a1d7e940202bde93344fb1d49b4
Marco <marco_trabajo@yahoo.com>
2021-12-08 12:19:14 +0100

    React JS Events - onClick, onMouseOver - passing parameters to functions

--- src/index.js
+++ src/index.js
@@ -33,12 +33,24 @@ function BookList(){
 }
 
 
-const Book = ({img, title, author}) => {       //you can pull the variables instead of props directly
+const Book = ({img, title, author}) => {      
+  // attribute, eventHandler 
+  // onClick,   onMouseOver 
+  const clickHandler = () => {            //Create the function to assign it to the element as reference
+    alert('hello world');
+  };
+
+  const functionTakingParameter = (author) => {
+    console.log(author);
+  };
+
   return (
-    <article className="book">
+    <article className="book" onMouseOver={ () => (console.log(title))}>
       <img src={img} alt="" />
-      <h1>{title}</h1>
+      <h1 onClick={ () => console.log()}>{title}</h1>
       <h4>{author}</h4>
+      <button type="button" onClick={clickHandler} >reference example</button>   {/*add the function name  = add function as reference */}
+      <button type="button" onClick={ () => functionTakingParameter(author)}>function taking parameters</button>
     </article>
   )
 }
