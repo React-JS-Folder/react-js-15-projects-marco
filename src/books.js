8679a9119f6688905c2761caf1a95cc6c50b4e8b
Marco <marco_trabajo@yahoo.com>
2021-12-08 13:55:46 +0100

    React JS - ES6 modules import/export with examples

---
+++ src/books.js
@@ -0,0 +1,16 @@
+export const books = [
+  {
+    id: 1,  //this is to create a key (react needs to register each map element)
+    img: 
+    'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title: 'I Love You to the Moon and Back',
+    author: 'Amelia Hepworth'
+  },
+  {
+    id: 2,  //this is to create a key (react needs to register each map element)
+    img: 
+      'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
+    title: 'Our Class is a Family',
+    author: 'Shannon Olsen'
+  }
+];
--- src/index.js
+++ src/index.js
@@ -1,25 +1,13 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'  
+import {books} from './books'
+import Book from './Book'
+import {greeting} from './testing/testing'
 
-const books = [
-  {
-    id: 1,  //this is to create a key (react needs to register each map element)
-    img: 
-    'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-    title: 'I Love You to the Moon and Back',
-    author: 'Amelia Hepworth'
-  },
-  {
-    id: 2,  //this is to create a key (react needs to register each map element)
-    img: 
-      'https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg',
-    title: 'Our Class is a Family',
-    author: 'Shannon Olsen'
-  }
-];
 
 function BookList(){
+  console.log(greeting);
   return(
     <section className="booklist">
       {books.map( (book) => {
@@ -33,27 +21,7 @@ function BookList(){
 }
 
 
-const Book = ({img, title, author}) => {      
-  // attribute, eventHandler 
-  // onClick,   onMouseOver 
-  const clickHandler = () => {            //Create the function to assign it to the element as reference
-    alert('hello world');
-  };
 
-  const functionTakingParameter = (author) => {
-    console.log(author);
-  };
-
-  return (
-    <article className="book" onMouseOver={ () => (console.log(title))}>
-      <img src={img} alt="" />
-      <h1 onClick={ () => console.log()}>{title}</h1>
-      <h4>{author}</h4>
-      <button type="button" onClick={clickHandler} >reference example</button>   {/*add the function name  = add function as reference */}
-      <button type="button" onClick={ () => functionTakingParameter(author)}>function taking parameters</button>
-    </article>
-  )
-}
 
 
 
