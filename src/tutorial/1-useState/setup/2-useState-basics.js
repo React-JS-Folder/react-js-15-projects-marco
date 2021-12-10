384ec48e911970296b2c8c0fc5cb02cba9300ed4
Marco <marco_trabajo@yahoo.com>
2021-12-10 12:31:27 +0100

    React JS Advanced - Hooks - useState - change content on click and condition

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  const [text, setText] = useState("Random Title");
+  const handleClick = () => {
+    setText("Welldone Marco!");    //reason to use react = render new content without reloading the page
+  }
+
+  const handleClick2 = () => {
+    if(text === "Random Title") {
+      setText("React JS - Winter in London!");
+    }else {
+      setText("Random Title");
+    }
+  }
+  return (
+    <React.Fragment>
+      <h3>useState basic example</h3>
+      <h4>{text}</h4>
+      <button className="btn" type="button" onClick={handleClick}> change title </button>
+      <button className="btn" type="button" onClick={handleClick2}>change title on condition</button>
+    </React.Fragment>
+
+
+  )
 };
 
 export default UseStateBasics;
