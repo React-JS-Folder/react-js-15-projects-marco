0d8be5053c74b64a67d82d0cc894c926f522f154
Marco <marco_trabajo@yahoo.com>
2021-12-10 14:58:30 +0100

    React JS - useState Hook - map - data from other file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react'
-import ErrorExample from './tutorial/1-useState/setup/1-error-example'
-import UseStateBasics from './tutorial/1-useState/setup/2-useState-basics'
+import UseStateArray from './tutorial/1-useState/setup/3-useState-array'
+
 function App() {
   return (
     <div className='container'>
-      {/* <ErrorExample /> */}
-      <UseStateBasics />
+      <UseStateArray />
     </div>
   )
 }
