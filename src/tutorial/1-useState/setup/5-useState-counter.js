04e823e66ce8f6a68f5a84ff3f89e71feb837a5f
Marco <marco_trabajo@yahoo.com>
2021-12-11 20:16:50 +0100

    React JS - Advanced - Counter App - setTimeout, prevState

--- src/tutorial/1-useState/setup/5-useState-counter.js
+++ src/tutorial/1-useState/setup/5-useState-counter.js
@@ -6,6 +6,14 @@ const UseStateCounter = () => {
   const reset = () => {
     setValue(0);
   }
+
+  const complexCounter = () => {
+    setTimeout( () => {
+      setValue((prevState) => {
+        return prevState + 1;
+      });
+    }, 2000);
+  }
   return (
     <>
       <section style={{ margin: '4rem 0' }}>
@@ -14,6 +22,11 @@ const UseStateCounter = () => {
         <button className="btn" onClick={() => setValue(value + 1)}>Increase</button>
         <button className="btn" onClick={reset}>Reset</button>
         <button className="btn" onClick={() => setValue(value - 1)}>Decrease</button>
+      </section>
+      <section style={{ margin: '4rem 0' }}>
+        <h2>complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexCounter}>complex counter</button>
       </section>
 
     </>
