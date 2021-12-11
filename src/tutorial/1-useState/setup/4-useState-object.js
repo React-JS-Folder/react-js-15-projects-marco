b16ca28a33ee5cc8602ac9d0af6a5f9700f8329e
Marco <marco_trabajo@yahoo.com>
2021-12-11 10:55:31 +0100

    React JS - Advanced - useState with objects as value vs  useState for each variable

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,48 @@
+// ################################################################
+// useState for an object 
+// import React, { useState } from 'react';
+
+// const UseStateObject = () => {
+//   const [person, setPerson] = useState({
+//     name: 'peter',
+//     age: 24,
+//     message: 'random message'
+//   })
+
+//   const changeMessage = () => {
+//     setPerson( {...person, message: 'hello world'})      //spread the object, property:'value'
+//   }
+
+//   console.log(person);
+//   return <>
+//     <h3>{person.name}</h3>       { /*make reference to the object.property */ }
+//     <h3>{person.age}</h3>
+//     <h3>{person.message}</h3>
+//     <button className="btn" onClick={changeMessage}>change message</button>
+//   </>
+// };
+
+// export default UseStateObject;
+
+// ################################################################
+// useState for each variable 
 import React, { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("sonic");
+  const [age, setAge] = useState(24);
+  const [message, setMessage] = useState("sonic is back");
+
+  const changeMessage = () => {
+    setMessage('hello world');
+  }
+
+  return <>
+    <h3>{name}</h3>
+    <h3>{age}</h3>
+    <h3>{message}</h3>
+    <button className="btn" onClick={changeMessage}>change message</button>
+  </>
 };
 
 export default UseStateObject;
