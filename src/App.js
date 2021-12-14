e0e8b7f6bf838913c41e2f332434d9ccce648bdb
Marco <marco_trabajo@yahoo.com>
2021-12-14 16:10:02 +0100

    React JS - Advanced - get Data using Fetch, useEffect, Map

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import UseEffectCleanup from './tutorial/2-useEffect/setup/2-useEffect-cleanup'
+import UseEffectFetchData from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
 
 
 function App() {
   return (
     <div className='container'>
-      <UseEffectCleanup />
+      <UseEffectFetchData />
 
     </div>
   )
