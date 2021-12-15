4e870a91deee675c03282a89023830001b19a875
Marco <marco_trabajo@yahoo.com>
2021-12-15 15:18:18 +0100

    React JS - Advanced - Ternary operator-easy example using useState

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -4,14 +4,18 @@ import React, { useState } from 'react';
 
 const ShortCircuit = () => {
   const [text, setText] = useState('');         //falsy value  (empty string)
-  const firstValue = text || 'hello world';     //it will be a value or other value
-  const secondValue = text && 'hello world';    //it will a value + other value
+  const [isError, setIsError] = useState(false);
 
   return (
     <>
       <h1>{text || 'john doe'}</h1>      {/*it will print out the variable if truthy or the string if falsy */} 
-      {text && <h1>hello world</h1>}     {/*it will print out the variable if truthy and the ......} */}
-      {!text && <h1>hello world</h1>}    {/* if variable is falsy it will print out the <h1>..</h1>} */}
+      <button className="btn" onClick={() => setIsError(!isError)}>toggle error</button>
+
+      {isError && <h1>Error...</h1>}
+      {isError ? <p>There is an error</p> : 
+      <div>
+        <h2>There is no error</h2>
+        </div>}
    </>
   )
 
