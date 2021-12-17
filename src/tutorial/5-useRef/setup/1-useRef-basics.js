18d8c300663a83f56dce74d3103ba8c4e392c9c7
Marco <marco_trabajo@yahoo.com>
2021-12-17 11:23:20 +0100

    React JS - Advanced - useRef hook gets html elements  content, including tags,  or input value

--- src/tutorial/5-useRef/setup/1-useRef-basics.js
+++ src/tutorial/5-useRef/setup/1-useRef-basics.js
@@ -1,11 +1,40 @@
-import React, { useEffect, useRef } from 'react';
-
+import React, { useEffect, useRef } from 'react'
 // preserves value
 // DOES NOT trigger re-render
 // target DOM nodes/elements
 
-const UseRefBasics = () => {
-  return <h2>useRef</h2>;
-};
+// form elements 
+// const refInput = useRef(nul);
+// refInput.current.value            //gets the value of form element
+// refInput.current.focus()          //get the form element with the cursor, ready to type
 
-export default UseRefBasics;
+const UseRefBasics = () => {
+  const refContainer = useRef(null);
+  const divContainer = useRef(null);
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(refContainer.current.value);           //form elements to get VALUE
+  }
+
+  useEffect( () => {
+      console.log(divContainer.current);               //any DOM element CONTENT (no only text)
+      refContainer.current.focus();    
+  })
+
+
+  return (
+    <>
+      <form className="form" onSubmit={handleSubmit}>
+        <div>
+          <input type="text" ref={refContainer}/>
+          <button type="submit"> submit </button>
+        </div>
+      </form>
+      <div ref={divContainer}>The article has been sold </div>
+    </>
+  )
+}
+
+export default UseRefBasics
